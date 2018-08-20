da3872d3389cb965ea74342ee5dd96dc6339c1bc
David Hart <dhartirl@gmail.com>
2018-08-21 00:41:53 +0100

    Allow heroku to set port

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     awsRoot: process.env.awsRoot || 'S3_ROOT_FOLDER',
     dbUrl: process.env.dbUrl || 'DBURL_HERE',
     clientLocation: process.env.clientLocation || 'http://localhost:4200',
-    listenPort: process.env.listenPort || 4201
+    listenPort: process.env.PORT || 4201
 }
